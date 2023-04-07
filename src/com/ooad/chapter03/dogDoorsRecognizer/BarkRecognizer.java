4ca08f993ef35dce032070787dc6b37d87694006
bhganesh <bharath110996@gmail.com>
2023-04-07 20:02:17 +0530

    OOAD chapter3 and chapter 4

---
+++ src/com/ooad/chapter03/dogDoorsRecognizer/BarkRecognizer.java
@@ -0,0 +1,16 @@
+package com.ooad.chapter03.dogDoorsRecognizer;
+
+public class BarkRecognizer {
+
+  private DogDoor door;
+
+  public BarkRecognizer(DogDoor door) {
+    this.door = door;
+  }
+
+  public void recognize(String bark) {
+    System.out.println("   BarkRecognizer: Heard a '" +
+        bark + "'");
+    door.open();
+  }
+}
