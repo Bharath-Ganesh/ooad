377e8a9396202c77909b8d44a7e6dfc930b790e0
bhganesh <bharath110996@gmail.com>
2023-04-07 20:20:50 +0530

    OOAD chapter 5, 6, 7 and 9

---
+++ src/com/ooad/chapter05/ricksGuitarsMandolins/Guitar.java
@@ -0,0 +1,8 @@
+package com.ooad.chapter05.ricksGuitarsMandolins;
+
+public class Guitar extends Instrument {
+ 
+  public Guitar(String serialNumber, double price, GuitarSpec spec) {
+    super(serialNumber, price, spec);
+  }
+}
